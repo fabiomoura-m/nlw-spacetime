b3e6a8bd8d2d0070133906bdb2d9314faef689cd
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-05-19 15:12:14 -0300

    config setup back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,19 @@
-const names:string = "oi"
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
-console.log(names)
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+  })
